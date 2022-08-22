726cf625b6ffe0aae394cd614cbc457c429c974a
Donald Brooks Arum <brooksd079@gmail.com>
2022-08-22 15:50:46 +0300

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 5, "M", "Brown", "Aggressive", TRUE);
+INSERT INTO bears VALUES (2, "Rowdy", 6, "M", "Black", "Aggressive", FALSE);
+INSERT INTO bears VALUES (3, "Tabitha", 5, "F", "Brown", "Aggressive", TRUE);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 3, "M", "Brown", "Docile", TRUE);
+INSERT INTO bears VALUES (5, "Melissa", 8, "F", "Brown", "Aggressive", TRUE);
+INSERT INTO bears VALUES (6, "Grinch", 7, "M", "Brown", "Docile", TRUE);
+INSERT INTO bears VALUES (7, "Wendy", 9, "M", "Brown", "Docile", FALSE);
+INSERT INTO bears VALUES (8, NULL, 10, "M", "Brown", "Aggressive", TRUE);
